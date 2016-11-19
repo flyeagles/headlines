1d4a27d9581445b72c4e48258fe94ce8a5afce6e
flyeagles <flyeagles_1@yahoo.com>
2016-11-19 10:26:34 +0800

    use get

--- headlines.py
+++ headlines.py
@@ -3,7 +3,7 @@
 import feedparser
 from flask import Flask
 from flask import render_template
-
+from flask import request
 
 app = Flask(__name__)
 
@@ -11,8 +11,13 @@ RSS_FEEDS={'baidu':'http://news.baidu.com/ns?word=shanghai&tn=newsrss&sr=0&cl=2&
 	'iol':'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route('/<publication>')
-def get_news(publication='baidu'):
+def get_news():
+	query = request.args.get("publication")
+	if not query or query.lower() not in RSS_FEEDS:
+		publication = 'baidu'
+	else:
+		publication = query.lower()
+
 	feed=feedparser.parse(RSS_FEEDS[publication])
 	return render_template("home.html",
 		arts=feed.entries )
